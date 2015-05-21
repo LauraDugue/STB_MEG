556ccd18ddfc2a47a3f0e0e7c9f7cd41c9d63394
LauraDugue <purpadmin@hypatia.cns.nyu.edu>
2015-05-21 12:04:10 -0400

    manual inspection

--- MEGanalysis.m
+++ MEGanalysis.m
@@ -3,8 +3,17 @@
 %      usage: MEGanalysis
 %         by: laura
 %       date: 21/05/15
-%    purpose: 
+%    purpose: this master program is used to run all sub-analysis program
+%             for the MEG analysis
 
+%% Add the path for fieldtrip and eeglab if necessary
+addpath(genpath('/users2/purpadmin/Laura/MRI/GitRepo/fieldtrip'));
+addpath(genpath('/users2/purpadmin/Laura/MRI/GitRepo/eeglab13_4_4b'));
 
-% addpath(genpath('/users2/purpadmin/Laura/MRI/GitRepo/fieldtrip'));
-% addpath(genpath('/users2/purpadmin/Laura/MRI/GitRepo/eeglab13_4_4b'));
+%% Run the pre-processing of the data
+
+ld_runMEGPreproc
+
+%% Manual Inspection through Fieldtrip
+
+ld_manualInspection
